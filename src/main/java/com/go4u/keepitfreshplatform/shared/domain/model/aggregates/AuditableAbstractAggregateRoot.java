4a476317f3b3c299435d0630ffa2f47fa8964f78
Werner Lang <u202310003@upc.edu.pe>
2025-06-23 11:33:14 -0500

    hi

--- src/main/java/com/go4u/keepitfreshplatform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
+++ src/main/java/com/go4u/keepitfreshplatform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -1,13 +1,13 @@
 package com.go4u.keepitfreshplatform.shared.domain.model.aggregates;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.domain.AbstractAggregateRoot;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import org.springframework.data.annotation.CreatedDate;
+//import org.springframework.data.annotation.LastModifiedDate;
+//import org.springframework.data.domain.AbstractAggregateRoot;
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.util.Date;
+//import java.util.Date;
 
 /**
  * Abstract class for aggregate roots that need auditing capabilities.
@@ -16,13 +16,13 @@ import java.util.Date;
  * @param <T> the type of the aggregate root
  */
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private Date createdAt;
+    //@CreatedDate
+    //@Column(nullable = false, updatable = false)
+    //private Date createdAt;
 
 
-    @LastModifiedDate
-    @Column(nullable = false)
-    private Date updatedAt;
+    //@LastModifiedDate
+    //@Column(nullable = false)
+    //private Date updatedAt;
 
 
